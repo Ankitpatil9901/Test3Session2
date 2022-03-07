b8860b6e8c657e575fc3c29dbb685d3f239f298c
Ankit Patil <96898434+Ankitpatil9901@users.noreply.github.com>
2022-03-07 07:09:52 +0000

    nothing

--- p2final.c
+++ p2final.c
@@ -1,38 +1,46 @@
 #include<stdio.h>
-int input_n_and_r(int *n,int *r)
+typedef struct fraction
 {
-  printf("Enter the value of n: ");
-  scanf("%d",n);
-  printf("Enter the value of r :");
-  scanf("%d",r);
+int num,den;
+}Fraction;
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("enter the fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
 }
-int ncr(int n,int r)
+Fraction Largest_fraction(Fraction f1,Fraction f2,Fraction f3 )
 {
-  int nfact=1,rfact=1,nrfact=1,ncr;     //nrfact is for (n-r)!
-  for(int i=1;i<=n;i++)
-    {
-      nfact*=i;
-    }
-  for(int j=1;j<=r;j++)
-    {
-      rfact*=j;
-    }
-  for(int k=1;k<=(n-r);k++)
-    {
-      nrfact*=k;
-    }
-  ncr=nfact/(rfact*nrfact);
-  return ncr;
+  int a,b,c;
+  int sden=f1.den*f2.den*f3.den;
+  a=f1.num*sden/f1.den;
+  b=f2.num*sden/f2.den;
+  c=f3.num*sden/f3.den;
+  if(a>b && a>c)
+  {
+    return f1;
+  }
+  else if(b>c && b>a)
+  {
+    return f2; 
+  }
+  else
+  {
+    return f3;
+  }
 }
-void output(int n,int r,int result)
+void output(Fraction f1,Fraction f2,Fraction f3,Fraction largest)
 {
-  printf("The factorial of %d and %d is %d",n,r,result);
+  printf("the largest of %d/%d , %d/%d and %d/%d is %d/%d",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den,largest.num,largest.den);
 }
 int main()
 {
-  int a,b,result;
-  input_n_and_r(&a,&b);
-  result=ncr(a,b);
-  output(a,b,result);
+  Fraction f1,f2,f3,largest;
+  f1=input_fraction();
+  f2=input_fraction();
+  f3=input_fraction();
+  largest=Largest_fraction(f1,f2,f3);
+  output(f1,f2,f3,largest);
   return 0;
-}
+  }
